8301b2f8ffa718a9c7f7f79b3903237875b3af00
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-12-22 03:08:33 +0100

    .

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,3 +1,11 @@
+import style from './HomePage.module.css';
+
 export default function HomePage() {
-    return 
-}
+    return (
+        <div className={style.container}>
+            <h1 className={style.title}>
+                Welcome to the phonebook app &#128214;
+            </h1>
+        </div>
+    );
+};
