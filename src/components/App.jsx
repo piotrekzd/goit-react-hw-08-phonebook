12e0df33be120dc509b80d894001730fc2c1933c
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-12-22 03:38:17 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,36 @@
 import React, { useEffect, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useAuth } from 'utils/hooks/useAuth';
+import { useDispatch } from 'react-redux';
+import { refreshUser } from 'redux/auth/operations';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { Loader } from './Loader/Loader';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
 
+const HomePage = lazy(() => import('../pages/HomePage'));
+const Register = lazy(() => import('../pages/Register'));
+const Login = lazy(() => import('../pages/Login'));
+const Contacts = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
-
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch])
 
-  return (
-    <div>
-      <h1 className={style.header}>Phonebook</h1>
-      <ContactForm />
-      <h2 className={style.header}>Contacts</h2>
-      <Filter />
-      {isLoading && !error ? <Loader /> : <ContactList />}
-    </div>
+  return isRefreshing ? (
+    <Loader />
+  ) : (
+    <Routes>
+      <Route path='/' element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='register' element={<RestrictedRoute redirectTo='/contacts' component={<Register />} />} />
+        <Route path='login' element={<RestrictedRoute redirectTo='/contacts' component={<Login />} />} />
+        <Route path='contacts' element={<PrivateRoute redirectTo='/login' component={<Contacts />} />} />
+      </Route>
+    </Routes>
   );
 };
